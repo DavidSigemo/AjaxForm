6931c9892c5a74dfbf8726d992d291c1ba1afce9
DavidSigemo <David.Sigemo@live.com>
2017-12-07 19:31:42 +0100

    Add project files.

---
+++ AjaxForm/Controllers/DefaultController.cs
@@ -0,0 +1,44 @@
+﻿using AjaxForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxForm.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public PartialViewResult Test()
+        {
+            List<TestViewModel> modelList = new List<TestViewModel>();
+
+            string letters = "ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvxyz0123456789!#%&/()=?@£$€{[]}\\";
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                string name = "";
+
+                for (int u = 0; u < 10; u++)
+                {
+                    name += letters.Substring(rand.Next(78), 1);
+                }
+
+                TestViewModel temp = new TestViewModel() { ID = i, Name = name };
+
+                modelList.Add(temp);
+            }
+
+            return PartialView(modelList);
+        }
+    }
+}
